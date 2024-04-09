cc66c547f7db0195ae6a2ed438a27ade46cd73d5
tomohiro_0225 <express22212270@gmail.com>
2024-04-09 11:03:29 +0900

    no message

---
+++ home.php
@@ -0,0 +1,749 @@
+    <?php get_header(); ?>
+    <!-- /header -->
+
+    <!-- main -->
+    <main>
+      <!-- fv -->
+      <section id="p-fv" class="p-fv">
+        <p class="p-fv__text">良い曲との出会いは人生を彩る</p>
+      </section>
+      <!-- /fv -->
+      
+      <!-- about -->
+      <section id="about" class="p-about">
+        <div class="p-about__inner">
+          <img src="<?php echo get_template_directory_uri(); ?>/img/about-left-up.png" alt="about-left-up" class="about-left-up">
+          <img src="<?php echo get_template_directory_uri(); ?>/img/about-right-up.png" alt="about-right-up" class="about-right-up">
+          <img src="<?php echo get_template_directory_uri(); ?>/img/about-right-down.png" alt="about-right-down" class="about-right-down">
+          <p class="p-about__text">誠和音芸は、代表坂田誠山が主宰するNPO法人ドルチェ邦楽合奏団の<br>
+                                  活動の中で生まれた作品を中心に、尺八や箏などの縦譜、及び五線譜の<br>
+                                  楽譜とCDの出版などを行ってます。<br><br>
+  
+                                  NPO法人ドルチェ邦楽合奏団は「邦楽って楽しいの？」「もちろん！」を<br>
+                                  モットーに活動しています。<br>
+                                  アマチュア主体の合奏団ですが、周年記念演奏会では2000人規模のホールが<br>
+                                  満杯の大盛況になるなど、多くの方からご好評頂いております。<br><br>
+  
+                                  邦楽愛好家の皆さんが楽しい曲に会って、自身も楽しみ、聴衆も楽しみ、<br>
+                                  このプラスのスパイラルが巻き起こる事でさらなる盛り上がりが見られるでしょう。<br><br>
+  
+                                  生まれた作品たちを通じて、あなたの日々を彩るお手伝いができましたら幸いです。
+            </p>
+
+        </div>
+      </section>
+      <!-- /about -->
+
+      <!-- menu -->
+      <section id="menu" class="p-menu">
+        <div class="p-menu__contents">
+          <!-- コンセプト -->
+          <a href="#" class="p-menu__item">
+            <div class="p-menu__circle --red">
+              <img src="<?php echo get_template_directory_uri(); ?>/img/shakuhachi.png" alt="shakuhachi" class="shakuhachi">
+              <p class="p-menu__name">コンセプト</p>
+            </div>
+          </a>
+          <!-- 楽譜・CD -->
+          <a href="#" class="p-menu__item">
+            <div class="p-menu__circle --green">
+              <img src="<?php echo get_template_directory_uri(); ?>/img/shamisen.png" alt="shamisen" class="shamisen">
+              <p class="p-menu__name">楽譜・CD</p>
+            </div>
+          </a>
+          <!-- ドルチェホール・利用案内 -->
+          <a href="#" class="p-menu__item">
+            <div class="p-menu__circle --pink">
+              <img src="<?php echo get_template_directory_uri(); ?>/img/koto.png" alt="koto" class="koto">
+              <p class="p-menu__name">ドルチェホール<br>
+                利用案内</p>
+            </div>
+          </a>
+        </div>
+      </section>
+      <!-- /menu -->
+
+      <!-- news -->
+      <section id="news" class="p-news">
+        <div class="p-news__content">
+          <h2 class="p-news__title">新着情報</h2>
+          <span class="p-news__underline"></span>
+  
+          <!-- 新着情報 -->
+          <div class="p-news__list">
+            <div class="p-news__item">
+              <p class="p-news__item__date">2024.04.15</p>
+              <p class="p-news__item__title">定期演奏会のお知らせ</p>
+            </div>
+            <div class="p-news__item">
+              <p class="p-news__item__date">2024.04.10</p>
+              <p class="p-news__item__title">予てから上級者向けで人気の「海鳴り」の縦譜が完成しました。</p>
+            </div>
+            <div class="p-news__item">
+              <p class="p-news__item__date">2024.04.07</p>
+              <p class="p-news__item__title">ホームページをリニューアルしました。</p>
+            </div>
+          </div>
+          <!-- 一覧ボタン -->
+          <a href="#" class="c-button news-button">
+            <div class="c-button__circle"></div>
+            <p class="c-button__name">お知らせ一覧</p>
+          </a>
+        </section>
+        <!-- /news -->
+
+      <!-- npo -->
+      <section id="npo" class="p-npo">
+        <!-- コンテンツ -->
+        <div class="p-npo__content">
+            <p class="p-npo__name">NPO法人ドルチェ邦楽合奏団</p>
+            <p class="p-npo__desc">邦楽界の活性化を目指して、<br>
+              地域の学校やコミュニティでの演奏、<br>
+              国内外での公演など幅広く活動しています。</p>
+              <!-- 一覧ボタン -->
+              <a href="#" class="c-button npo-button">
+                <div class="c-button__circle"></div>
+                <p class="c-button__name">くわしく見る</p>
+              </a>
+        </div>
+
+        <!-- コンテンツ -->
+        <div class="p-npo__content">
+            <p class="p-npo__name">邦楽創造集団　オーラJ</p>
+            <p class="p-npo__desc">日本の各種伝統楽器による新しい作品を作し、 演奏する<br>
+              優秀な人材を団員として、日本国内だけでなく国際的に<br>
+              活動をしている専門的な創造集団です。</p>
+              <!-- 一覧ボタン -->
+              <a href="#" class="c-button npo-button">
+                <div class="c-button__circle"></div>
+                <p class="c-button__name">くわしく見る</p>
+              </a>
+        </div>
+      </section>
+
+        <!-- お問い合わせ -->
+        <section class="p-contact">
+          <div class="p-contact__inner">
+            <p class="p-contact__text">お問い合わせ・<br>
+              楽譜、CDのご注文はこちら</p>
+            <a href="043-241-3142" class="p-contact__tel --sp">TEL: 043-241-3142</a>
+            <p class="p-contact__tel --pc">TEL: 043-241-3142</p>
+            <p class="p-contact__time">受付時間／9:00～18:00</p>
+            <a href="" class="p-contact__button">✉  お問合せ／ご注文フォーム</a>
+          </div>
+        </section>
+        <!-- /お問い合わせ -->
+
+    </main>
+    <!-- /main -->
+
+    <!-- footer -->
+    <?php get_footer(); ?>
+    <!-- /footer -->
+
+    <!-- css -->
+    <style>
+      /* html */
+      html section{
+        color: #2F2B26;
+      }
+      main{
+        background-color: #EFEFE6;
+      }
+      /* fv */
+      .p-fv {
+        width: 100%;
+        height: 100vh;
+        background-color: aqua;
+      }
+
+      .p-fv__text {
+        font-family: "游明朝体", "Yu Mincho", YuMincho, "ヒラギノ明朝 Pro", "Hiragino Mincho Pro", "MS P明朝", "MS PMincho", serif;
+        text-align: center;
+        font-size: 16px;
+      }
+      @media screen and (max-width:768px) {}
+
+      /* about */
+      .p-about {
+        margin-top: 45px;
+      }
+
+      .p-about__inner{
+        width: 1200px;
+        margin: 0 auto;
+        padding: 28px 0 57px;
+        position: relative;
+      }
+      .about-left-up {
+        width: 368px;
+        height: 352px;
+        position: absolute;
+        top: 00%;
+        left: 0%;
+      }
+
+      .about-right-up {
+        width: 232px;
+        height: 193px;
+        position: absolute;
+        top: 00%;
+        right: 0%;
+      }
+
+      .about-right-down {
+        width: 358px;
+        height: 338px;
+        position: absolute;
+        bottom: 00%;
+        right: 0%;
+      }
+
+      .p-about__text {
+        text-align: center;
+        font-family: "游明朝体", "Yu Mincho", YuMincho, "ヒラギノ明朝 Pro", "Hiragino Mincho Pro", "MS P明朝", "MS PMincho", serif;
+        font-size: 26px;
+        line-height: 44px;
+        color: #2F2B26;
+        position: relative;
+      }
+      
+      @media screen and (max-width:768px) {
+      /* about */
+      .p-about {
+        margin-top: 45px;
+      }
+
+      .p-about__inner{
+        width: 1200px;
+        margin: 0 auto;
+        width: calc(100% - 40px);
+        padding: 28px 20px 57px;
+        position: relative;
+      }
+      .about-left-up {
+        width: 184px;
+        height: 176px;
+        position: absolute;
+        top: 00%;
+        left: 0%;
+      }
+
+      .about-right-up {
+        width: calc(232px / 2);
+        height: calc(193px / 2);
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      }
+
+      .about-right-down {
+        width: 179px;
+        height: 159px;
+        position: absolute;
+        bottom: 0%;
+        right: 0%;
+      }
+
+      .p-about__text {
+        text-align: left;
+        font-size: 20px;
+        line-height: 30px;
+      }
+    }
+
+      /* menu */
+      .p-menu {
+        max-width: 1088px;
+        width: 100%;
+        height: 353px;
+        margin: 80px auto 0;
+      }
+
+      .p-menu__contents {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+      }
+
+      .p-menu__item {
+        width: 304px;
+        margin-right: 88px;
+      }
+      .p-menu__item:last-child {
+        margin-right: 0px;
+      }
+
+
+      .p-menu__circle {
+        margin-top: 49px;
+        width: 304px;
+        height: 304px;
+        border-radius: 304px;
+        position: relative;
+      }
+
+      .--red {
+        background-color: #AB3B3A;
+      }
+      .--red:hover{
+        background-color: #da6765;
+      }
+      .--red:hover img {
+        transform:translate(-50%, -50%) scale(1.1);
+      }
+
+      .shakuhachi {
+        width: 144px;
+        height: 105px;
+        position: absolute;
+        top: 0%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        transition: all 0.3s;
+      }
+
+      .p-menu__name {
+        font-family: "游明朝体", "Yu Mincho", YuMincho, "ヒラギノ明朝 Pro", "Hiragino Mincho Pro", "MS P明朝", "MS PMincho", serif;
+        font-size: 34px;
+        line-height: 44px;
+        white-space: nowrap;
+        color: #fff;
+        text-align: center;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+      }
+
+      .--green {
+        background-color: #838A2D;
+        transition: all 0.3s;
+      }
+      .--green:hover {
+        background-color: #b0b94d;
+      }
+      .--green:hover img {
+        transform:translate(-50%, -50%) scale(1.1);
+      }
+
+      .shamisen {
+        width: 184px;
+        height: 142px;
+        position: absolute;
+        top: 0%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        transition: all 0.3s;
+
+      }
+
+      .--pink {
+        background-color: #EB7A77;
+      }
+      .--pink:hover {
+        background-color: #f5a4a2;
+      }
+      .--pink:hover img {
+        transform:translate(-50%, -50%) scale(1.1);
+      }
+
+      .koto {
+        width: 206px;
+        height: 73px;
+        position: absolute;
+        top: 0%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        transition: all 0.3s;
+      }
+      @media screen and (max-width:768px) {
+          /* menu */
+        .p-menu {
+          max-width: 1088px;
+          padding: 0 20px;
+          width: calc(100% - 40px);
+          height: max-content;
+          margin: 17px auto 0;
+        }
+
+        .p-menu__contents {
+          display: block;
+        }
+
+        .p-menu__item {
+          width: 304px;
+          padding: 60px 0 0;
+          margin: 0px auto 0;
+        }
+        .p-menu__item:last-child {
+          margin-right: auto;
+          margin-top: 0px;
+        }
+
+
+        .p-menu__circle {
+          margin-top: 49px;
+          width: 304px;
+          height: 304px;
+          border-radius: 304px;
+          position: relative;
+        }
+
+        .--red {
+          background-color: #AB3B3A;
+        }
+
+        .shakuhachi {
+          width: 144px;
+          height: 105px;
+          position: absolute;
+          top: 0%;
+          left: 50%;
+          transform: translate(-50%, -50%);
+        }
+
+        .p-menu__name {
+          font-family: "游明朝体", "Yu Mincho", YuMincho, "ヒラギノ明朝 Pro", "Hiragino Mincho Pro", "MS P明朝", "MS PMincho", serif;
+          font-size: 34px;
+          line-height: 44px;
+          white-space: nowrap;
+          color: #fff;
+          text-align: center;
+          position: absolute;
+          top: 50%;
+          left: 50%;
+          transform: translate(-50%, -50%);
+        }
+
+        .--green {
+          background-color: #838A2D;
+        }
+
+        .shamisen {
+          width: 184px;
+          height: 142px;
+          position: absolute;
+          top: 0%;
+          left: 50%;
+          transform: translate(-50%, -50%);
+
+        }
+
+        .--pink {
+          background-color: #EB7A77;
+        }
+
+        .koto {
+          width: 206px;
+          height: 73px;
+          position: absolute;
+          top: 0%;
+          left: 50%;
+          transform: translate(-50%, -50%);
+        }
+      }
+
+      /* news */
+      .p-news {
+        margin-top: 90px;
+      }
+
+      .p-news__content {
+        max-width: 904px;
+        margin: 0 auto;
+      }
+
+      .p-news__title {
+        margin-bottom: 24px;
+        font-family: "游明朝体", "Yu Mincho", YuMincho, "ヒラギノ明朝 Pro", "Hiragino Mincho Pro", "MS P明朝", "MS PMincho", serif;
+        font-size: 42px;
+        text-align: center;
+        color: #333;
+      }
+
+      .p-news__underline {
+        display: block;
+        margin: 0 auto 54px;
+        width: 54px;
+        height: 1px;
+        background-color: #707070;
+      }
+
+      .p-news__list {
+        border-top: solid 1px #333;
+
+      }
+
+      .p-news__item {
+        display: flex;
+        align-items: center;
+        justify-content: left;
+        width: 100%;
+        border-bottom: solid 1px #333;
+      }
+
+      .p-news__item__date {
+        padding: 40px 129px 41px 20px;
+        font-family: "游明朝体", "Yu Mincho", YuMincho, "ヒラギノ明朝 Pro", "Hiragino Mincho Pro", "MS P明朝", "MS PMincho", serif;
+        font-size: 22px;
+        color: #333;
+      }
+
+      .p-news__item__title {
+        padding: 40px 0px 41px;
+        font-family: "游明朝体", "Yu Mincho", YuMincho, "ヒラギノ明朝 Pro", "Hiragino Mincho Pro", "MS P明朝", "MS PMincho", serif;
+        font-size: 22px;
+        color: #333;
+
+      }
+      @media screen and (max-width:768px) {}
+
+      /* c-button */
+      .c-button {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        position: relative;
+        width: fit-content;
+      }
+      .news-button{
+        margin: 52px 0 0 auto;
+      }
+
+      .c-button__circle {
+        width: 70px;
+        height: 70px;
+        border-radius: 35px;
+        background-color: #EB7A77;
+      }
+
+      .c-button__name {
+        font-family: "游明朝体", "Yu Mincho", YuMincho, "ヒラギノ明朝 Pro", "Hiragino Mincho Pro", "MS P明朝", "MS PMincho", serif;
+        font-size: 24px;
+        color: #333;
+        white-space: nowrap;
+        position: absolute;
+        top: 50%;
+        left: 100%;
+        transform: translate(-15%, -50%);
+      }
+      @media screen and (max-width:768px) { /* c-button */
+      .c-button {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        position: relative;
+        width: fit-content;
+      }
+      .news-button{
+        margin: 52px 0 0 auto;
+      }
+
+      .c-button__circle {
+        width: 40px;
+        height: 40px;
+        border-radius: 20px;
+      }
+
+      .c-button__name {
+        font-family: "游明朝体", "Yu Mincho", YuMincho, "ヒラギノ明朝 Pro", "Hiragino Mincho Pro", "MS P明朝", "MS PMincho", serif;
+        font-size: 16px;
+        color: #333;
+        white-space: nowrap;
+        position: absolute;
+        top: 50%;
+        left: 100%;
+        transform: translate(-15%, -50%);
+      }
+    }
+
+      /* npo */
+      .p-npo {
+        max-width: 1088px;
+        margin: 80px auto 0;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+      }
+
+      .p-npo__content {
+        max-width: 516px;
+        background-color: #FBFCF2;
+        padding: 40px;
+      }
+      .p-npo__content:last-child {
+        margin-left: 55px;
+      }
+
+      .p-npo__name {
+        font-family: "游明朝体", "Yu Mincho", YuMincho, "ヒラギノ明朝 Pro", "Hiragino Mincho Pro", "MS P明朝", "MS PMincho", serif;
+        font-size: 30px;
+        color: #333;
+
+      }
+
+      .p-npo__desc {
+        margin-top: 20px;
+        font-family: "游明朝体", "Yu Mincho", YuMincho, "ヒラギノ明朝 Pro", "Hiragino Mincho Pro", "MS P明朝", "MS PMincho", serif;
+        font-size: 17px;
+        line-height: 29px;
+        color: #333;
+
+      }
+
+      .c-button.npo-button {
+        margin: 41px 90px 0 auto;
+
+      }
+      @media screen and (max-width:991px) {
+      /* npo */
+      .p-npo {
+        max-width: 544px;
+        padding: 0 20px;
+        margin: 60px auto 0;
+        display: block;
+      }
+
+      .p-npo__content {
+        max-width: 516px;
+        background-color: #FBFCF2;
+        padding: 20px;
+      }
+      .p-npo__content:last-child {
+        margin-left: 0px;
+        margin-top: 30px;
+      }
+
+      .p-npo__name {
+        font-family: "游明朝体", "Yu Mincho", YuMincho, "ヒラギノ明朝 Pro", "Hiragino Mincho Pro", "MS P明朝", "MS PMincho", serif;
+        font-size: 22px;
+        color: #333;
+
+      }
+
+      .p-npo__desc {
+        margin-top: 15px;
+        font-size: 13px;
+        line-height: 20px;
+
+      }
+
+      .c-button.npo-button {
+        margin: 30px 110px 0 auto;
+
+      }
+
+      }
+
+      /* お問い合わせ */
+      .p-contact {
+        margin-top: 80px;
+        background-image: url(./img/contact-bg.png);
+        background-size: 100% 100%;
+        background-repeat: no-repeat;
+        background-position: center;
+        width: 100%;
+      }
+
+      .p-contact__inner {
+        padding: 62px 0 81px;
+      }
+
+      .p-contact__text {
+        padding: 0 0 60px;
+        font-family: "游明朝体", "Yu Mincho", YuMincho, "ヒラギノ明朝 Pro", "Hiragino Mincho Pro", "MS P明朝", "MS PMincho", serif;
+        font-size: 36px;
+        color: #fff;
+        text-align: center;
+        text-shadow: 0px 3px 10px rgba(00, 00, 00, 0.7);
+      }
+
+      .p-contact__tel {
+        margin: 0 0 18px;
+        font-family: "游明朝体", "Yu Mincho", YuMincho, "ヒラギノ明朝 Pro", "Hiragino Mincho Pro", "MS P明朝", "MS PMincho", serif;
+        font-size: 46px;
+        color: #fff;
+        text-align: center;
+        text-shadow: 3px 3px 10px rgba(00, 00, 00, 0.7);
+      }
+      .p-contact__tel.--pc{
+        display: block;
+      }
+      .p-contact__tel.--sp{
+        display: none;
+      }
+
+      .p-contact__time {
+        margin-bottom: 41px;
+        font-family: "游明朝体", "Yu Mincho", YuMincho, "ヒラギノ明朝 Pro", "Hiragino Mincho Pro", "MS P明朝", "MS PMincho", serif;
+        font-size: 18px;
+        color: #fff;
+        text-align: center;
+        text-shadow: 3px 3px 10px rgba(00, 00, 00, 0.7);
+
+      }
+
+      .p-contact__button {
+        font-family: "游明朝体", "Yu Mincho", YuMincho, "ヒラギノ明朝 Pro", "Hiragino Mincho Pro", "MS P明朝", "MS PMincho", serif;
+        padding: 23px 62px 29px 63px;
+        margin: 0 auto;
+        background-color: #fff;
+        color: #8A701A;
+        font-size: 22px;
+        width: fit-content;
+        box-shadow: 3px 5px 10px rgba(00, 00, 00, 0.6);
+      }
+
+      @media screen and (max-width:768px) {
+      /* お問い合わせ */
+      .p-contact {
+        margin-top: 60px;
+      }
+
+      .p-contact__inner {
+        padding: 40px 0;
+      }
+
+      .p-contact__text {
+        padding: 0 0 40px;
+        font-size: 25px;
+      }
+
+      .p-contact__tel {
+        margin: 0 0 15px;
+        font-size: 30px;
+      }
+      .p-contact__tel.--pc{
+        display: none;
+      }
+      .p-contact__tel.--sp{
+        display: block;
+      }
+
+      .p-contact__time {
+        margin-bottom: 30px;
+        font-size: 14px;
+      }
+
+      .p-contact__button {
+        padding: 20px 40px;
+        font-size: 20px;
+      }
+    }
+
+
+
+
+    </style>
+
+    <!-- jquery/js -->
+    <script
+  src="https://code.jquery.com/jquery-3.7.1.js"
+  integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4="
+  crossorigin="anonymous"></script>
+    <script src="./assets/js/common.js"></script>
+    
+</body>
+</html>
